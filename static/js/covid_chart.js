2a9c8853c628a9b3deee0ae9b05b802a150e38b8
Siara Leininger <leinings@oregonstate.edu>
2021-02-06 14:17:33 -0800

    Starting visualization work.

---
+++ static/js/covid_chart.js
@@ -0,0 +1,25 @@
+// Function when the PPE dropdown value changes
+function optionChanged(product) {
+    buildTimeChart(product);
+}
+
+// Function to build the time series chart for the selected product
+function buildTimeChart(product) {
+    // Create variables to save the data to
+
+    // Read in COVID data and save to variables
+
+    // Read in Product data and save to variables
+
+    // Create traces and layout
+
+    // Build time series chart
+}
+
+function init() {
+    var dropdown = ds.select("selPPE");
+
+    // Figure out how to populate dropdown with unique values from data.
+
+
+}
