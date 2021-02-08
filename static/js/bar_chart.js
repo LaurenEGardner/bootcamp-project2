6f92cd35feb16cdae32e1435863518671a614f65
staceywai <staceywai@Staceys-MacBook-Pro.local>
2021-02-08 14:11:10 -0800

    update bar-chart js

---
+++ static/js/bar_chart.js
@@ -0,0 +1,26 @@
+// Create function for Data plotting (Bar, bubble)
+// function DataPlots(bar_chart) {
+
+//url for export 2020
+url_e20 = "http://127.0.0.1:5000/export2019"
+
+//read in json files
+// d3.json(url_e20), function (d) {
+//     // d3.csv("Resources/data/exportData2020.csv").then(function(exportdata){
+//     return{
+//         commodity: d.COMMODITY_DESCRIPTION,
+//         year: d.YEAR,
+//         month: d.MONTH,
+//         country: d.COUNTRY_NAME,
+//         total_export: +d.ALL_VALUES_YEAR
+//     };
+//     }).then(function(data{
+//         console.log(data[0])
+//     })
+
+
+
+        // log list of names
+        // var commodities = data.map(data=>data.COMMODITY_DESCRIPTION);
+        // console.log("commodities",commodities);
+
