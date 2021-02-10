9020ae4db210317d0e90a110a08bbd7eb19b95d8
Siara Leininger <leinings@oregonstate.edu>
2021-02-09 20:24:26 -0800

    Working on chart

--- static/js/covid_chart.js
+++ static/js/covid_chart.js
@@ -1,41 +1,93 @@
 // Function when the PPE dropdown value changes
-function productChanged(product) {
-    var radio = d3.select("input[name='infoType']:checked").node().value;
-    buildTimeChart(product, radio);
+function productChanged(productCode) {
+    console.log("Changed product", productCode);
+    var infoType = d3.select("input[name='infoType']:checked").node().value;
+    getData(productCode, infoType);
 }
 
 function infoTypeChanged() {
-    var dropdown = d3.select("#selProduct").node().value;
-    var radio = d3.select("input[name='infoType']:checked").node().value;
-    buildTimeChart(dropdown, radio);
+    var productCode = d3.select("#selProduct").node().value;
+    var infoType = d3.select("input[name='infoType']:checked").node().value;
+    console.log("Info change", productCode, productName, radio);
+    getData(productCode, infoType);
 }
 
 // Function to build the time series chart for the selected product
-function buildTimeChart(product, infoType) {
-    console.log(product, infoType);
+function getData(productCode, infoType) {
+    console.log(productCode, productName, infoType);
     // Create variables to save the data to
     var productData = {};
     var covidData = {};
+    var url = "";
 
-    d3.json("/export2019totals/3002").then((data) => {
+    if (infoType == "Import") {
+        url = "/import2020totals/" + productCode;
+        console.log("Import URL", url);
+    } else {
+        url = "/export2020totals/" + productCode;
+        console.log("Export URL", url)
+    }
+
+    // Read in Product data and save to variables
+    d3.json(url).then((data) => {
         console.log(data, "Inside Product D3");
         productData = data;
     });
+
+    // Read in COVID data and save to variables
     d3.json("/covid2020").then((data) => {
         console.log(data, "Inside COVID D3");
         covidData = data;
     });
 
+    // Give API calls time
     setTimeout(() => { console.log(productData, "Outside Product D3"); }, 2000);
-    console.log(covidData, "Outside COVID D3");
+    setTimeout(() => { console.log(covidData, "Outside COVID D3"); }, 2000);
+    setTimeout(() => { buildCovidChart(productCode, infoType, covidData, productData); }, 2000);
+}
 
-    // Read in COVID data and save to variables
-
-    // Read in Product data and save to variables
-
+function buildCovidChart(productCode, infoType, covidData, productData) {
     // Create traces and layout
+    console.log("Inside chart ", productData);
+
+    // X axis values (Months will be the same for each graph.)
+    var months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+    // Save COVID Data
+    var productInfo = productData.map(row => row.ALL_VALUES_MONTH);
+    console.log("Product Information: ", productInfo);
+
+    var trace1 = {
+        x: months,
+        y: productData.map(row => row.ALL_VALUES_MONTH),
+        name: `${productName} ${infoType}`,
+        type: 'scatter'
+      };
+      
+      var trace2 = {
+        x: months,
+        y: covidData.map(row => row.cases),
+        name: 'COVID Cases',
+        yaxis: 'y2',
+        type: 'scatter'
+      };
+      
+      var data = [trace1, trace2];
+      
+      var layout = {
+        title: `COVID Cases vs. US ${infoType}s`,
+        yaxis: {title: `${productName} ${infoType} Volume`},
+        yaxis2: {
+          title: 'COVID Cases',
+          titlefont: {color: 'rgb(148, 103, 189)'},
+          tickfont: {color: 'rgb(148, 103, 189)'},
+          overlaying: 'y',
+          side: 'right'
+        }
+      };
 
     // Build time series chart
+    Plotly.newPlot('covidInfo', data, layout);
 }
 
 function init() {
@@ -43,7 +95,7 @@ function init() {
     var radio = d3.select("input[name='infoType']:checked").node().value;
 
     // Populate drop down
-    var productOptions = [{value: 1, text: "One"}, {value: 2, text: "Two"}, {value: 3, text: "Three"}];
+    var productOptions = [{value: 3002, text: "Vaccines, Blood, and Immunological products"}, {value: 3003, text: "Medication (unmeasured doses)"}, {value: 3004, text: "Medication (measured doses)"}, {value: 3005, text: "Medically-treated Bandages"}, {value: 3006, text: "Surgical Equipment"}];
 
     var menuOptions = d3.select("#selProduct")
                         .selectAll("options")
@@ -55,7 +107,7 @@ function init() {
                         
 
     // Build chart with default info
-    buildTimeChart(productOptions[0], radio);
+    getData(productOptions[0].value, radio);
 
 }
 
